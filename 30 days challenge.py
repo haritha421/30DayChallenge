7fe99d609b620de821f4e0870275b21192b21e19
haritha421 <54301887+haritha421@users.noreply.github.com>
2019-08-20 10:19:49 +0530

    uploaded day 1 task -planning

---
+++ 30 days challenge.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#AGENDA:
+    #1.extract the website using web scraping using python
+    #2.use libraries of python to manipulate the dataset
+    #3.feed the updated data in a data frame
+    #4.come up with new data frame that differ the original
+#GOAL:
+    #use and test of python libraries and its efficiency to maximum with the scraping data
+
+
+# In[ ]:
+
+
+#using num py and pandas to create a data frame
+import numpy as np
+import pandas as pd
+DF=pd.DataFrame()
+
+
+# In[ ]:
+
+
+
+
